0c08d12d81ee22b2e564a2d22b2ef8b0e777d8fb
Marko Tintor <marko@tintor.net>
2025-01-24 10:47:51 -0800

    geometry test refactor for xrational

--- algebra/segment_segment_squared_distance_test.cc
+++ algebra/segment_segment_squared_distance_test.cc
@@ -3,45 +3,48 @@
 #include "algebra/__test.h"
 using std::array;
 
+using M = xrational;
+using V = Vec3<M>;
+
 TEST_CASE("segment_segment_squared_distance - line vs line") {
-    REQUIRE(segment_segment_squared_distance<rational>({0,0,0}, {5,0,0}, {3,3,8}, {3,-3,8}) == 64);
+    REQUIRE(segment_segment_squared_distance<M>({0,0,0}, {5,0,0}, {3,3,8}, {3,-3,8}) == 64);
 }
 
 TEST_CASE("segment_segment_squared_distance - parallel") {
-    qvec3 a(0,0,0), b(5,0,0), d(1,2,0);
+    V a(0,0,0), b(5,0,0), d(1,2,0);
     REQUIRE(segment_segment_squared_distance(a, b, a + d, b + d) == 4);
 }
 
 TEST_CASE("segment_segment_squared_distance - colinear") {
-    REQUIRE(segment_segment_squared_distance<rational>({0,0,0}, {5,0,0}, {7,0,0}, {10,0,0}) == 4);
+    REQUIRE(segment_segment_squared_distance<M>({0,0,0}, {5,0,0}, {7,0,0}, {10,0,0}) == 4);
 }
 
 TEST_CASE("segment_segment_squared_distance - identical") {
-    REQUIRE(segment_segment_squared_distance<rational>({0,0,0}, {5,0,0}, {0,0,0}, {5,0,0}) == 0);
+    REQUIRE(segment_segment_squared_distance<M>({0,0,0}, {5,0,0}, {0,0,0}, {5,0,0}) == 0);
 }
 
 TEST_CASE("segment_segment_squared_distance - overlap") {
-    REQUIRE(segment_segment_squared_distance<rational>({0,0,0}, {5,0,0}, {5,0,0}, {4,0,0}) == 0);
+    REQUIRE(segment_segment_squared_distance<M>({0,0,0}, {5,0,0}, {5,0,0}, {4,0,0}) == 0);
 }
 
 TEST_CASE("segment_segment_squared_distance - vertex vs vertex") {
-    REQUIRE(segment_segment_squared_distance<rational>({0,0,0}, {5,0,0}, {6,1,0}, {6,9,0}) == 2);
+    REQUIRE(segment_segment_squared_distance<M>({0,0,0}, {5,0,0}, {6,1,0}, {6,9,0}) == 2);
 }
 
 TEST_CASE("segment_segment_squared_distance - vertex vs line") {
-    REQUIRE(segment_segment_squared_distance<rational>({0,0,0}, {5,0,0}, {1,2,0}, {1,9,0}) == 4);
+    REQUIRE(segment_segment_squared_distance<M>({0,0,0}, {5,0,0}, {1,2,0}, {1,9,0}) == 4);
 }
 
 TEST_CASE("failing") {
-    qvec3 a={-97, -68, 74};
-    qvec3 b={54, 73, -33};
-    qvec3 c={-6, -59, -74};
-    qvec3 d={-77, 61, -35};
+    V a={-97, -68, 74};
+    V b={54, 73, -33};
+    V c={-6, -59, -74};
+    V d={-77, 61, -35};
     const auto e = segment_segment_squared_distance(a, b, c, d);
     REQUIRE(segment_segment_squared_distance(c, d, b, a) == e);
 }
 
-void REQUIRE_segment_segment_squared_distance(qvec3 a, qvec3 b, qvec3 c, qvec3 d, rational e, const array<qvec3, 4>& orig) {
+void REQUIRE_segment_segment_squared_distance(V a, V b, V c, V d, M e, const array<V, 4>& orig) {
     auto o = segment_segment_squared_distance(a, b, c, d);
     if (o != e) {
         std::print("segment_segment_squared_distance({} | {} | {} | {}) = {}\n", a, b, c, d, o);
@@ -50,7 +53,7 @@ void REQUIRE_segment_segment_squared_distance(qvec3 a, qvec3 b, qvec3 c, qvec3 d
     REQUIRE(o == e);
 }
 
-void test_segment_segment_squared_distance(qvec3 a, qvec3 b, qvec3 c, qvec3 d, rational e, const array<qvec3, 4>& orig) {
+void test_segment_segment_squared_distance(V a, V b, V c, V d, M e, const array<V, 4>& orig) {
     REQUIRE_segment_segment_squared_distance(a, b, c, d, e, orig);
     REQUIRE_segment_segment_squared_distance(b, a, c, d, e, orig);
     REQUIRE_segment_segment_squared_distance(a, b, d, c, e, orig);
@@ -62,15 +65,15 @@ void test_segment_segment_squared_distance(qvec3 a, qvec3 b, qvec3 c, qvec3 d, r
     REQUIRE_segment_segment_squared_distance(d, c, b, a, e, orig);
 }
 
-rational SampleQ(Random& rng) { return rational{rng.Uniform<int>(-1000, 1000), rng.Uniform<int>(1, 1000)}; }
-qvec3 SampleQV3(Random& rng) { return {SampleQ(rng), SampleQ(rng), SampleQ(rng)}; }
+M SampleQ(Random& rng) { return {rng.Uniform<int>(-1000, 1000), rng.Uniform<int>(1, 1000)}; }
+V SampleQV3(Random& rng) { return {SampleQ(rng), SampleQ(rng), SampleQ(rng)}; }
 
-void test_segment_segment_squared_distance2(qvec3 a, qvec3 b, qvec3 c, qvec3 d, Random& rng) {
+void test_segment_segment_squared_distance2(V a, V b, V c, V d, Random& rng) {
     const auto e = segment_segment_squared_distance(a, b, c, d);
-    array<qvec3, 4> orig = {a, b, c, d};
+    array<V, 4> orig = {a, b, c, d};
 
     for (int i = 0; i < 6; i++) {
-        auto conv = [i](qvec3 v) {
+        auto conv = [i](auto v) {
             if (i == 0) return v;
             if (i == 1) return xzy(v);
             if (i == 2) return yxz(v);
